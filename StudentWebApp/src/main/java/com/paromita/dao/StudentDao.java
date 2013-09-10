2578ae365723698038cac606ffa399b7476356f6
Paromita Banerjee <paromita.kol@gmail.com>
2013-09-10 23:54:24 +0530

    StudentApp - Sample solution to integrate Spring + Maven + Hibernate + MySql

---
+++ StudentWebApp/src/main/java/com/paromita/dao/StudentDao.java
@@ -0,0 +1,14 @@
+package com.paromita.dao;
+
+import java.util.List;
+
+import com.paromita.model.Student;
+
+public interface StudentDao {
+	
+	public void add(Student student);
+	public void edit(Student student);
+	public void delete(int studentId);
+	public Student getStudent(int studentId);
+	public List getAllStudents();	
+}
---
+++ StudentWebApp/src/main/java/com/paromita/service/impl/StudentServiceImpl.java
@@ -0,0 +1,44 @@
+package com.paromita.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.paromita.dao.StudentDao;
+import com.paromita.model.Student;
+import com.paromita.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+	@Autowired
+	private StudentDao studentDao;
+	
+	@Transactional
+	public void add(Student student) {
+		studentDao.add(student);
+	}
+
+	@Transactional
+	public void edit(Student student) {
+		studentDao.edit(student);
+	}
+
+	@Transactional
+	public void delete(int studentId) {
+		studentDao.delete(studentId);
+	}
+
+	@Transactional
+	public Student getStudent(int studentId) {
+		return studentDao.getStudent(studentId);
+	}
+
+	@Transactional
+	public List getAllStudents() {
+		return studentDao.getAllStudents();
+	}
+
+}
