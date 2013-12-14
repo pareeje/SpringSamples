0c06835d74fa2c8c4bd3153e170e9f42c160b5ed
paromita <paromita.kol@gmail.com>
2013-12-14 15:24:52 +0530

    Setting msg property in xml

--- springdemo/src/main/java/paromita/com/springdemo/Triangle.java
+++ springdemo/src/main/java/paromita/com/springdemo/Triangle.java
@@ -2,8 +2,20 @@ package paromita.com.springdemo;
 
 public class Triangle {
 	
+	private String msg;
+	
 	public void draw(){
-		System.out.println("Triangle Draw");
+		System.out.println(msg);
 	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
 
 }
