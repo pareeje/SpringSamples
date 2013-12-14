ededde53384413680591ac3ec424d7a8ecc5c769
paromita <paromita.kol@gmail.com>
2013-12-14 15:33:44 +0530

    Using constructor args

--- springdemo/src/main/java/paromita/com/springdemo/Triangle.java
+++ springdemo/src/main/java/paromita/com/springdemo/Triangle.java
@@ -3,9 +3,19 @@ package paromita.com.springdemo;
 public class Triangle {
 	
 	private String msg;
+	private String type;
+	
+	public Triangle(String type){
+		this.type = type;
+	}
 	
 	public void draw(){
-		System.out.println(msg);
+		System.out.println(getType() + " " + getMsg());
+	}
+
+	
+	public String getType() {
+		return type;
 	}
 
 	public String getMsg() {
